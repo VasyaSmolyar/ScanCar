c54fba1ddb0ffc3f8616a1b504280e1afec4a38c
Vasily Smolyar <smolyar4@gmail.com>
2020-10-09 12:51:29 +0300

    Добавлены кнопки

--- App.js
+++ App.js
@@ -1,6 +1,7 @@
 import { StatusBar } from 'expo-status-bar';
+import * as Font from 'expo-font';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import 'react-native-gesture-handler';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
@@ -9,6 +10,16 @@ import CameraScreen from './components/CameraScreen';
 const Stack = createStackNavigator();
 
 export default function App() {
+	const [loaded, error] = Font.useFonts({
+		SFPro: require('./assets/SFPro.ttf'),
+	});
+
+	if(!loaded) {
+		return (
+			<Text>{error}</Text>
+		)
+	}
+
 	return (
 		<View style={{flex: 1}}>
 			<StatusBar style="auto" />
--- modals/CarModal.js
+++ modals/CarModal.js
@@ -13,10 +13,19 @@ export default function CarModal({item}) {
             <View style={styles.backContainer}>
                 <View style={{flex: 1}}></View>
                 <View style={styles.container}>
+                    <View style={styles.carContainer}>
 
+                    </View>
+                    <View style={styles.creditContainer}>
+                        <TouchableOpacity style={styles.buyButton}>
+                            <Text style={styles.buyText}>Купить новую</Text>
+                        </TouchableOpacity>
+                        <TouchableOpacity style={styles.creditButton}>
+                            <Text style={styles.creditText}>Взять в кредит</Text>
+                        </TouchableOpacity>
+                    </View>
                 </View>
                 <View style={styles.bottomContainer}>
-                    
 
                 </View>
             </View>
@@ -27,7 +36,9 @@ export default function CarModal({item}) {
 const styles = StyleSheet.create({
 	backContainer: {
         flex: 1,
-        backgroundColor: 'rgba(0, 0, 0, 0.3)'
+        backgroundColor: 'rgba(0, 0, 0, 0.3)',
+        paddingHorizontal: 15,
+        width: '100%'
     },
     container: {
         flex: 5,
@@ -37,4 +48,39 @@ const styles = StyleSheet.create({
     bottomContainer: {
         flex: 1
     },
+    carContainer: {
+        flex: 2
+    },
+    creditContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        paddingHorizontal: 15
+    },
+    buyButton: {
+        width: '100%',
+        borderRadius: 10,
+        backgroundColor: '#3a83f1',
+        alignItems: 'center',
+        paddingVertical: 15,
+        marginBottom: 25
+    },
+    buyText: {
+        fontFamily: 'SFPro',
+        textTransform: "uppercase",
+        fontSize: 15,
+        letterSpacing: 1.5,
+        color: '#fff'
+    },
+    creditButton: {
+        width: '100%',
+        alignItems: 'center',
+        paddingVertical: 15
+    },
+    creditText: {
+        fontFamily: 'SFPro',
+        textTransform: "uppercase",
+        fontSize: 15,
+        letterSpacing: 1.5,
+        color: '#3a83f1'
+    }
 });
